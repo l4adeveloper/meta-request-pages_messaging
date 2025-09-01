5f34bb9e52611812ab44c725bd9dfde907b5f7aa
l4adeveloper <88945248+l4adeveloper@users.noreply.github.com>
2025-09-01 12:20:40 +0700

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4444;
 
 // --- Middleware ---
 const corsOptions = {
